bfda1be92d152a4100fe6bff94855d3954d00b51
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-02-24 12:12:35 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      space:['JetBrains Mono', 'monospace'],
+      bitter:['Bitter', 'serif'],
+    }
+  },
+  plugins: [],
+}
