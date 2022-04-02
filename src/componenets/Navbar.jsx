c9641941945d194624dc15b78065b703c88fa189
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-04-02 13:21:35 +0530

    More responsiveness

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='shadow pt-4 pb-4 flex flex-col sm:flex-row justify-center items-center gap-x-96  ' >
+    <div className='shadow pt-4 pb-4 flex flex-col md:flex-row justify-center items-center gap-x-96  ' >
       <h1 className='text-4xl font-bold flex justify-center  font-space tracking-widest'>
         &lt;CODEKARO&#47;&gt;
       </h1>
